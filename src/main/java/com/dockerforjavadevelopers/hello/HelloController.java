84949d5a83cc0e11e49388c62dcae79891894778
Soumya06in <53549658+Soumya06in@users.noreply.github.com>
2020-08-19 00:17:19 +0530

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello World\n";
+        return "Hi, Hola, Hallo, Hej all are same!!!!\n";
     }
     
 }
